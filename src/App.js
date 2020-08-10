89cce8a333bae9eb88f8e029ff7fd5856e7f2d6e
kharioki <antolisah@gmail.com>
2020-08-10 22:34:43 +0300

    figure component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Figure from './components/Figure';
 
 function App() {
   return (
     <>
       <Header />
+      <Figure />
     </>
   );
 }
