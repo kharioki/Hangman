a520e8320a8ba2a1924e3da1a385eb3ec9a1a205
kharioki <antolisah@gmail.com>
2020-08-10 23:19:30 +0300

    wrongletters, word, popup and notification component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Figure from './components/Figure';
+import WrongLetters from './components/WrongLetters';
+import Word from './components/Word';
+import Popup from './components/Popup';
+import Notification from './components/Notification';
 
 function App() {
   return (
     <>
       <Header />
-      <Figure />
+      <div className="game-container">
+        <Figure />
+        <WrongLetters />
+        <Word />
+      </div>
+      <Popup />
+      <Notification />
     </>
   );
 }
